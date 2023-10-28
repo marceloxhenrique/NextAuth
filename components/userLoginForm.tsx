16cb63ad5e675020bc71e35f24ececfea45a027c
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-10-28 23:23:28 +0200

    feat: Create a user login form

---
+++ components/userLoginForm.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+import { useRouter } from "next/navigation";
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface IUser {
+  email: string;
+  password: string;
+}
+
+export function UserLoginForm({ className, ...props }: UserAuthFormProps) {
+  const router = useRouter();
+  const [data, setData] = useState<IUser>({
+    email: "",
+    password: "",
+  });
+
+  async function onSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    setData({ email: "", password: "" });
+    setIsLoading(false);
+  }
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleChange = () => {};
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="password"
+              autoCorrect="off"
+              disabled={isLoading}
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            Log in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
