72f97ed7664d145ce20873837fbe4c8021f78844
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-01 00:17:55 +0100

    feat: update prisma/schema and add CredentialsProvider in auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,61 @@
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "User name" },
+        password: { label: "password", type: "password" },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+      },
+      async authorize(credentials, req): Promise<any> {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Login data is obligatory");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("User not found");
+        }
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword as string
+        );
+        if (!matchPassword) {
+          throw new Error("Email and  password not valid");
+        }
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
+  pages: {
+    signIn: "/login",
+  },
+};
