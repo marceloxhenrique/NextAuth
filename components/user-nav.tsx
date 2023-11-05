a4f47b13dcbe505c55823f04960365a049f15e6c
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-05 16:59:10 +0100

    feat: Create a SessionProvider and update the avatar dropdown menu in the navbar

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -13,8 +13,10 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useSession } from "next-auth/react";
 
 export function UserNav() {
+  const { data: session, status } = useSession();
   const handleLogoutButton = async () => {
     await signOut();
   };
@@ -25,16 +27,18 @@ export function UserNav() {
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-9 w-9">
               <AvatarImage src="/avatars/03.png" alt="@shadcn" />
-              <AvatarFallback>SC</AvatarFallback>
+              <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-sm font-medium leading-none">
+                {session?.user?.name}
+              </p>
               <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
+                {session?.user?.email}
               </p>
             </div>
           </DropdownMenuLabel>
